d8661191dfc0b1476fc46b5dba54e9ffcc79158d
Андрей Зуев <andrej.zuev.2918@mail.ru>
2025-01-23 16:15:43 +0500

    some changes

---
+++ Controllers/BookController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Readify.Data;
+
+namespace Readify.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MoreInfo(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .Include(b => b.UploadedByUser)
+                .Include(b => b.Comments)
+                    .ThenInclude(c => c.User)
+                .FirstOrDefaultAsync(m => m.BookId == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+    }
+}
